634b7a6a2d123197a30f3bb2fb39a5ccdc11b3fc
Luxuan Cao <lcao@f6r11s5.clusters.42paris.fr>
2026-03-12 16:57:24 +0100

    pollout added

--- HandleClientPollout.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef HANDLECLIENTPOLLOUT_HPP
-# define HANDLECLIENTPOLLOUT_HPP
-# include "Server.hpp"
-# include "User.hpp"
-
-void handleClientPollOut(Server &irc, User &userManager, size_t &i);
-
-#endif
