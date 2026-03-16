329636f39215a3052e4a1ed70a5e782394fc0f0a
Luxuan Cao <lcao@f6r6s9.clusters.42paris.fr>
2026-03-16 14:15:00 +0100

    Add User.cpp executeCommand User function

--- User.cpp
+++ User.cpp
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   User.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: suna <suna@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: lcao <lcao@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 16:26:53 by suna              #+#    #+#             */
-/*   Updated: 2026/03/16 13:24:23 by suna             ###   ########.fr       */
+/*   Updated: 2026/03/16 14:14:05 by lcao             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "User.hpp"
 #include <utility>
+#include <iostream>
 
 std::vector<std::string> split(std::string str, std::string std)
 {
@@ -121,7 +122,23 @@ void User::executeCommand(int fd, std::string cmd)
         else if (cmds[0] == "NICK")
             ;//nick funtion
         else if (cmds[0] == "USER")
-            ;//user function
+        {   
+            //USER <username> <hostname> <servername> :<realname>
+            if(cmds.size() < 5)
+            {
+                std::string errorMsg = ":server 461 " + cmds[0] + " :Not enough parameters\r\n";
+                m_User_int[fd].writeBuffer += errorMsg;
+                return;
+            }
+            m_User_int[fd].loginName = cmds[1];
+            m_User_int[fd].hostName = cmds[2];
+            if(isLogin(fd))
+            {
+                std::string successMsg = ":server 001 " + m_User_int[fd].nickName + " :Welcome to the IRC server\r\n";
+                m_User_int[fd].writeBuffer += successMsg;
+                std::cout << "User " << m_User_int[fd].nickName << " logged in successfully." << std::endl;
+            }
+        }
             
     }
         
