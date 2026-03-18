dcb2525a30891f24614a235016f2dd7a27fdf79c
Luxuan Cao <lcao@f4r6s5.clusters.42paris.fr>
2026-03-18 13:59:22 +0100

    commands.hpp added topic, kick, mode

--- commands.hpp
+++ commands.hpp
@@ -6,7 +6,7 @@
 /*   By: lcao <lcao@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/16 13:37:07 by suna              #+#    #+#             */
-/*   Updated: 2026/03/16 16:03:33 by lcao             ###   ########.fr       */
+/*   Updated: 2026/03/18 13:58:34 by lcao             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,9 @@
     
     void nick(User &user, std::vector<std::string> cmd, int fd);
     void userCmd(User &userObj, std::vector<std::string> cmds, int fd);
+    void modeCmd(Channel &chanObj, User &userObj, const std::vector<std::string> &cmds, int fd);
+    void kickCmd(Channel &chanObj, User &userObj, const std::vector<std::string> &cmds, int fd);
+    void topicCmd(Channel &chanObj, User &userObj, const std::vector<std::string> &cmds, int fd);
+    
 
 #endif
