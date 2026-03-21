12a0dd19058b55d98a85a31cbba12c32eea4449f
Sungbin Na <suna@f4r11s11.clusters.42paris.fr>
2026-03-21 15:04:08 +0100

    cleaning duplicated code

--- User.cpp
+++ User.cpp
@@ -6,7 +6,7 @@
 /*   By: suna <suna@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 16:26:53 by suna              #+#    #+#             */
-/*   Updated: 2026/03/21 14:53:45 by suna             ###   ########.fr       */
+/*   Updated: 2026/03/21 15:02:51 by suna             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -172,6 +172,9 @@ void User::executeCommand(int fd, std::string cmd, std::string serverPass, Chann
             kickCmd(channel, *this, cmds, fd);
         else if (cmds[0] == "MODE")
             modeCmd(channel, *this, cmds, fd);
+        else
+            m_User_int[fd].writeBuffer += ":server 421 " + m_User_int[fd].nickName + " :Unkown command\r\n";
+            
     }
     else
     {
@@ -181,9 +184,14 @@ void User::executeCommand(int fd, std::string cmd, std::string serverPass, Chann
             nick(*this, cmds, fd);
         else if (cmds[0] == "USER")
             userCmd(*this, cmds, fd);
+        else
+            m_User_int[fd].writeBuffer += ":server 421 " + m_User_int[fd].nickName + " :Unkown command\r\n";
         
         if (isLogin(fd)){
             std::string welcome = ":server 001 " + m_User_int[fd].nickName + " :Welcome to the IRC server!\r\n";
+            std::string welcome = ":server 002 " + m_User_int[fd].nickName + " :Your host is " + m_User_int[fd].hostName + ", running version 1.0\r\n";
+            std::string welcome = ":server 003 " + m_User_int[fd].nickName + " :This server was created today\r\n";
+            std::string welcome = ":server 004 " + m_User_int[fd].nickName + " " + "ft_irc v:1.0\r\n";
             m_User_int[fd].writeBuffer += welcome;
             m_User_int[fd].status = 1; //change the status as connected
 
